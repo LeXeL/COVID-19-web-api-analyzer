8975363e5c8319a4452c7376ea31c876e0629c8f
Diego Rodriguez <lexelEZ@gmail.com>
2020-03-27 02:03:05 -0500

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,87 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <el-row>
+        <loader v-if="isLoading"></loader>
+        <div v-if="!isLoading">
+            <el-select :span="24" v-model="selectedCountry" placeholder="Countrys">
+                <el-option
+                    v-for="item in countrys"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                ></el-option>
+            </el-select>
+            <el-button @click="fetchSelectedCountry()" type="primary" icon="el-icon-search">Search</el-button>
+            <el-row>
+                <div id="chart"></div>
+            </el-row>
+            <el-row v-if="fetchData.length">
+                <el-collapse>
+                    <el-collapse-item title="Table Data" name="2">
+                        <el-table :data="fetchData" style="width: 100%">
+                            <el-table-column prop="date" label="Date" width="180"></el-table-column>
+                            <el-table-column prop="confirmed" label="Confirmed" width="180"></el-table-column>
+                            <el-table-column prop="recovered" label="Recovered"></el-table-column>
+                            <el-table-column prop="deaths" label="Deaths"></el-table-column>
+                        </el-table>
+                    </el-collapse-item>
+                </el-collapse>
+            </el-row>
+        </div>
+    </el-row>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import loader from '@/components/loader'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'home',
+    components: {
+        loader,
+    },
+    data() {
+        return {
+            countrys: [],
+            informationData: [],
+            selectedCountry: '',
+            isCollapse: true,
+            isLoading: false,
+            fetchData: [],
+        }
+    },
+    methods: {
+        fetchSelectedCountry() {
+            this.informationData[this.selectedCountry].forEach(
+                ({date, confirmed, recovered, deaths}) =>
+                    this.fetchData.push({
+                        date,
+                        confirmed,
+                        recovered,
+                        deaths,
+                    })
+            )
+        },
+    },
+    beforeCreate() {
+        this.isLoading = true
+        fetch('https://pomber.github.io/covid19/timeseries.json')
+            .then(response => response.json())
+            .then(data => {
+                this.informationData = data
+                this.countrys = Object.keys(data).map(v => {
+                    return {
+                        value: v,
+                        label: v,
+                    }
+                })
+            })
+            .then((this.isLoading = false))
+    },
 }
 </script>
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+}
+</style>
